e209f95ea975737ea0d86669d328a16dead1ab20
vaishak101 <nairvaishak101@gmail.com>
2022-01-12 22:18:59 +0530

    CUSTOM BIKES SECTION DONE

--- js/script.js
+++ js/script.js
@@ -21,3 +21,45 @@ ftContainer.addEventListener('click', function (e) {
     return;
   }
 });
+//SLIDER
+const sliderBtnLeft = document.querySelector('.slider__btn--left');
+const sliderBtnRight = document.querySelector('.slider__btn--right');
+
+const slide = document.querySelectorAll('.slide');
+let curSlide = 0;
+let maxSlide = slide.length - 1;
+
+const slider = function (cursl) {
+  slide.forEach(
+    (s, i) => (s.style.transform = `translate(${100 * (i - cursl)}%)`)
+  );
+};
+const nextSlide = function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  slider(curSlide);
+};
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  slider(curSlide);
+};
+
+slider(0);
+
+sliderBtnLeft.addEventListener('click', prevSlide);
+sliderBtnRight.addEventListener('click', nextSlide);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowLeft') {
+    prevSlide();
+  } else if (e.key === 'ArrowRight') {
+    nextSlide();
+  }
+});
